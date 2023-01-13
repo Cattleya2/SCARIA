7918f069967b110aa8dddda11765adf43d9f2c2f
Enock Seth Nyamador <enock.nyamador@tum.de>
2023-01-13 12:26:03 +0100

    update links and key

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_AY2UV8S0IjMZ281Utv5LUsx21cgpY6gTN9zXo5YjK0/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyA7ggEizFVQg_Q0_U5Gpjbk6hpv0tul8XI';
