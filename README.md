ce7bbf934e7cba2ba1499f561bab523a077ce8b1
Cattleya2 <59752729+Cattleya2@users.noreply.github.com>
2023-05-16 07:37:16 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-# Online Gender Based Violence map
-
-![Preview](preview.jpg)
-## Live links
-- Online Map: [here](https://mobilewebghana.github.io/ogbv-map/)
-- Data: [Google Sheets](https://docs.google.com/spreadsheets/d/1_AY2UV8S0IjMZ281Utv5LUsx21cgpY6gTN9zXo5YjK0/edit#gid=0)
+# SCARIA
 
 ## Credits
 - Based on [Leaflet Maps with Google Sheets](https://github.com/HandsOnDataViz/leaflet-maps-with-google-sheets)
--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_AY2UV8S0IjMZ281Utv5LUsx21cgpY6gTN9zXo5YjK0/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1JYiEei8p2fA-vRB_aifLScTR3nEzQDgL0Uy32YmK3oc/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyA7ggEizFVQg_Q0_U5Gpjbk6hpv0tul8XI';
